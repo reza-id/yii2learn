b80791876ed0597b4e484589e73ec853043b5468
Reza Agustiana <reza.agustiana@gmail.com>
2015-07-15 12:21:39 +0700

    13 add datepicker to form

--- backend/views/companies/_form.php
+++ backend/views/companies/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use dosamigos\datepicker\DatePicker;
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\Companies */
@@ -17,6 +18,18 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'company_email')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'company_address')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'company_start_date')->widget(
+	    DatePicker::className(), [
+	        // inline too, not bad
+	         'inline' => false, 
+	         // modify template for custom rendering
+	        //'template' => '<div class="well well-sm" style="background-color: #fff; width:250px">{input}</div>',
+	        'clientOptions' => [
+	            'autoclose' => true,
+	            'format' => 'yyyy-mm-dd'
+	        ]
+	]);?>
 
     <?= $form->field($model, 'company_status')->dropDownList([ 'active' => 'Active', 'inactive' => 'Inactive', ], ['prompt' => 'Status']) ?>
 
