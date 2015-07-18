3b089648a1b929ad395e7f7f8537bacb36b0a2a1
Reza Agustiana <reza.agustiana@gmail.com>
2015-07-18 11:34:22 +0700

    24 crud generation by gii
    CREATE TABLE `customers` (
     `customer_id` int(11) NOT NULL AUTO_INCREMENT,
     `customer_name` varchar(100) NOT NULL,
     `zip_code` varchar(20) NOT NULL,
     `city` varchar(100) NOT NULL,
     `province` varchar(100) NOT NULL,
     PRIMARY KEY (`customer_id`)
    );
    
    CREATE TABLE `locations` (
     `location_id` int(11) NOT NULL AUTO_INCREMENT,
     `zip_code` varchar(20) NOT NULL,
     `city` varchar(100) NOT NULL,
     `province` varchar(100) NOT NULL,
     PRIMARY KEY (`location_id`)
    );

---
+++ backend/views/customers/_form.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\Customers */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="customers-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'customer_name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'zip_code')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'city')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'province')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
